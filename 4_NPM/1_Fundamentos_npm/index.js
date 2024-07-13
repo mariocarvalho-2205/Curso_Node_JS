ef9a9b6f00108e47576e49f0a4c6b7bbc8341f2d
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2024-07-13 09:41:29 -0300

    npm 01 fundamentos

---
+++ 4_NPM/1_Fundamentos_npm/index.js
@@ -0,0 +1,9 @@
+/*
+* NPM = É o gerenciador de pacotes do NOdeJs
+* NPM = Node Package Manage
+* é possivel configurar projetos e rodar scripts por meio do npm
+* a criação de um projeto gere sempre um arquivo chamado package.json
+
+** comando init - inicia um projeto
+
+*/
