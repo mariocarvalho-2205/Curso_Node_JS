99dfe9837ba137f0f233bdd7acaf96d0c8655958
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2024-08-29 15:11:48 -0300

    resgatando dados

---
+++ Aulas/7_Node_e_MySQL/3_resgatar_dados/index.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const mysql = require('mysql')
+
+const port = 3000
+
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.post('/books/insertbook/', (req, res) => {
+    const title = req.body.title
+    const pageqty = req.body.pageqty
+
+    // create query
+    const query = `INSERT INTO books (title, pageqty) VALUE ('${title}', '${pageqty}')`
+
+    conn.query(query, function(err) {
+        if (err) {
+            console.log(err)
+            return
+        }
+
+        res.redirect('/books')
+    })
+})
+
+app.get('/books', (req, res) => {
+    const sql = "SELECT * FROM books"
+
+    conn.query(sql, function(err, data) {
+        if (err) { 
+            console.log(err)
+            return
+        }
+
+        const books = data
+
+        console.log(books)
+        res.render('books', {books})
+    })
+})
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodemysql'
+})
+
+conn.connect(function(err) {
+    if(err) {
+        console.log(err)
+    }
+
+    console.log(`Conectou ao MySQL Resgatando`)
+
+    app.listen(port, () => {
+        console.log(`Resgatando dados na porta ${port}`)
+    })
+})
