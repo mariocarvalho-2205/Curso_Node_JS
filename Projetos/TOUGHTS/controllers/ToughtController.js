fcb94e9d1aa4f0d3bead137103240ec51ee28826
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2024-10-16 11:55:56 -0300

    remove toughts

--- Projetos/TOUGHTS/controllers/ToughtController.js
+++ Projetos/TOUGHTS/controllers/ToughtController.js
@@ -7,12 +7,92 @@ module.exports = class ToughtsController {
     }
     
     static async dashboard (req, res) {
-        await res.render('toughts/dashboard')
+        const userid = req.session.userid
+
+        // verificando se usuario existe
+        const user = await User.findOne({
+            where: {
+                id: userid
+            },
+            include: Tought,  // recebe os oensamento que tem o usuario incluso
+            plain: true, // recebe so os dados interessantes
+        })
+
+        if(!user) {
+            res.redirect('/login')
+        }
+
+        // tratando os dados para rec3eber somente o dataValues
+
+        const toughts = user.Toughts.map((result) => result.dataValues)
+
+        let emptyToughts = false
+
+        if (toughts.length === 0 ) {
+            emptyToughts = true
+        }
+
+        console.table(toughts)
+
+
+        res.render('toughts/dashboard', {toughts, emptyToughts})
     }
 
     static createToughts(req, res) {
         res.render('toughts/create')
 
 
+    }
+
+    static async createToughtsSave(req, res) {
+        
+        try {
+            const tought = {
+                title: req.body.title,
+                UserId: req.session.userid
+            }
+            console.log(tought)
+            await Tought.create(tought)
+    
+
+            req.flash('message', 'Pensamento criado com sucesso!')
+    
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard')
+            })
+            
+        } catch (error) {
+            console.log(error, 'error no createtoughtsave')
+        }
+
+    }
+
+    // static editeditToughts (req, res) {
+    //     res.render('toughts/edit')
+    // }
+
+    static async removeToughts (req, res) {
+        // pegando id da rota
+        const id = req.body.id
+        const UserId = req.session.userid
+
+        try {
+            await Tought.destroy({
+                where: {id: id, UserId: UserId}
+            })
+
+            req.flash('message', 'Pensamento removido com sucesso!')
+    
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard')
+            })
+
+
+        } catch (error) {
+            console.log(error)
+        }
+
+
+        console.log(id)
     }
 }
--- Projetos/TOUGHTS/routes/toughtsRoutes.js
+++ Projetos/TOUGHTS/routes/toughtsRoutes.js
@@ -5,7 +5,11 @@ const ToughsController = require('../controllers/ToughtController')
 const checkAuth = require('../helpers/auth').checkAuth
 
 router.get('/add', checkAuth, ToughsController.createToughts)
+router.post('/add', checkAuth, ToughsController.createToughtsSave)
+
 router.get('/dashboard', checkAuth, ToughsController.dashboard)
+router.post('/remove', checkAuth, ToughsController.removeToughts)
+// router.get('/edit', checkAuth, ToughsController.editToughts)
 router.get('/', ToughsController.showToughts)
 
 module.exports = router
