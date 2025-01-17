8c64059fb9662cb0b438005293eb41814799b8fe
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2025-01-17 20:34:36 -0300

    fesat: creating page and function login

--- Projetos/GET_A_PET/frontend/src/hooks/userAuth.jsx
+++ Projetos/GET_A_PET/frontend/src/hooks/userAuth.jsx
@@ -5,67 +5,90 @@ import { useNavigate } from "react-router-dom"; // manipula as urls e enviando o
 import useFlashMessage from "../hooks/useFlashMessage";
 
 export default function useAuth() {
-  const [ authenticated, setAutheticated ] = useState(false)
-  const { setFlashMessage } = useFlashMessage(); // aqui chama a função do flash message
-  const navigate = useNavigate();
+	const [authenticated, setAutheticated] = useState(false);
+	const { setFlashMessage } = useFlashMessage(); // aqui chama a função do flash message
+	const navigate = useNavigate();
 
-  // inserir o token automatico na api quando o usuario acessa uma pagina
-  useEffect(() => {
-    const token = localStorage.getItem("token")
+	// inserir o token automatico na api quando o usuario acessa uma pagina
+	useEffect(() => {
+		const token = localStorage.getItem("token");
 
-    if (token) {
-      api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`  // envia o token para a api
-      setAutheticated(true)  // muda o estado do authenticated
-    }
-  }, [])
+		if (token) {
+			api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`; // envia o token para a api
+			setAutheticated(true); // muda o estado do authenticated
+		}
+	}, []);
 
+	async function register(user) {
+		const msgText = "Cadastro realizado com sucesso!";
+		const msgType = "sucess";
+		// console.log("User auth",user)  // ok chegou
 
-  async function register(user) {
-    const msgText = "Cadastro realizado com sucesso!";
-    const msgType = "sucess";
-    // console.log("User auth",user)  // ok chegou
+		try {
+			const response = await api.post("/users/register", user);
+			const data = await response.data;
 
-    try {
-      const response = await api.post("/users/register", user);
-      const data = await response.data;
+			setFlashMessage(msgText, msgType); // aqui e passado a mensagem para o hook flashmessage
 
-      setFlashMessage(msgText, msgType); // aqui e passado a mensagem para o hook flashmessage
-      
-      // console.log("contextoauth", data.token); // ok chegou
+			// console.log("contextoauth", data.token); // ok chegou
 
-      await authUser(data)  // chama a função para salvar o usuario no local storage e navegar para a rota barra
-    } catch (error) {
-      // console.error(error);  // ok chegou
-      const msgText = error.response.data.message;
-      const msgType = "error";
-      setFlashMessage(msgText, msgType); // aqui e passado a mensagem para o hook flashmessage
-    }
-  }
+			await authUser(data); // chama a função para salvar o usuario no local storage e navegar para a rota barra
+		} catch (error) {
+			// console.error(error);  // ok chegou
+			const msgText = error.response.data.message;
+			const msgType = "error";
+			setFlashMessage(msgText, msgType); // aqui e passado a mensagem para o hook flashmessage
+		}
+	}
 
-  async function authUser(data) {  // função para salvar o usuario no local storage e navegar para a rota barra
-    setAutheticated(true)  // muda o estado do authenticated
-    localStorage.setItem("token", JSON.stringify(data.token))  //salva o token no localstorage
+	// login function
+	async function login(user) {
+		let msgText = "Login realizado com sucesso!";
+		let msgType = "sucess";
+    // console.log("login auth", user)  // chegou
 
-    navigate('/')
-  }
+		try {
+			const response = await api.post("/users/login", user);
+			const data = await response.data;
+      // console.log("data", data)  // chegou
 
-  // logout funciont
-  function logout () {
-    // atualiza o estado das mensagens
-    const msgText = "Logout realizado com sucesso!"
-    const msgType = "sucess"
-    setFlashMessage(msgText, msgType)  // e envia a mensagem atualizada para a home
+			setFlashMessage(msgText, msgType); // aqui e passado a mensagem para o hook flashmessage
 
-    // muda o estado do authenticated e remove o token do local storage
-    setAutheticated(false)
-    localStorage.removeItem("token")
-    api.defaults.headers.Authorization = undefined  // e atribui o valor de undefined a api
-    navigate('/') // direciona para a home
-  }
+			await authUser(data); // chama a função para salvar o usuario no local storage e navegar para a rota barra
+		} catch (error) {
+      console.log(error)
+			msgText = error.response.data.message;
+			msgType = "error";
+      setFlashMessage(msgText, msgType)
+		}
+	}
 
-  return {
-    authenticated,
-    register,
-    logout
-  };
+	// logout funciont
+	function logout() {
+		// atualiza o estado das mensagens
+		const msgText = "Logout realizado com sucesso!";
+		const msgType = "sucess";
+		setFlashMessage(msgText, msgType); // e envia a mensagem atualizada para a home
+
+		// muda o estado do authenticated e remove o token do local storage
+		setAutheticated(false);
+		localStorage.removeItem("token");
+		api.defaults.headers.Authorization = undefined; // e atribui o valor de undefined a api
+		navigate("/"); // direciona para a home
+	}
+
+	async function authUser(data) {
+		// função para salvar o usuario no local storage e navegar para a rota barra
+		setAutheticated(true); // muda o estado do authenticated
+		localStorage.setItem("token", JSON.stringify(data.token)); //salva o token no localstorage
+
+		navigate("/");
+	}
+
+	return {
+		authenticated,
+		register,
+		logout,
+    login
+	};
 }
--- Projetos/GET_A_PET/frontend/src/pages/Login/Login.jsx
+++ Projetos/GET_A_PET/frontend/src/pages/Login/Login.jsx
@@ -1,11 +1,46 @@
-import React from "react";
+import { useState, useContext } from "react";
+import Input from "../../components/Input/Input";
+import styles from "../../components/Input/Form.module.css";
+import { Link } from 'react-router-dom'
 
+
+import { Context } from "../../context/UseContext";
 const Login = () => {
-  return (
-    <div>
-      <h1>Login</h1>
-    </div>
-  );
+	const [user, setUser] = useState({});
+	const { login } = useContext(Context);
+
+	function handleChange(e) {
+		setUser({ ...user, [e.target.name]: e.target.value });
+	}
+
+	function handleSubmit(e) {
+		e.preventDefault();
+    login(user)
+	}
+	return (
+		<section className={styles.form_container}>
+			<h1>login</h1>
+			<form onSubmit={handleSubmit}>
+				<Input
+					text="E-mail"
+					type="email"
+					name="email"
+					placeholder="Digite o seu email"
+					handleOnChange={handleChange}
+				/>
+				<Input
+					text="Senha"
+					type="password"
+					name="password"
+					placeholder="Digite sua senha"
+					handleOnChange={handleChange}
+				/>
+				<input type="submit" value="Entrar" />
+			</form>
+      <p>Não tem conta? <Link to="/register">Clique aqui</Link></p>
+
+		</section>
+	);
 };
 
 export default Login;
