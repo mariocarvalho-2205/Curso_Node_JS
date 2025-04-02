fdf5a67bdffd9e057659b003ca3c20647e6a7deb
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2025-04-02 14:34:19 -0300

    feat: add saving pet form

--- Projetos/GET_A_PET/frontend/src/components/PetForm/PetForm.jsx
+++ Projetos/GET_A_PET/frontend/src/components/PetForm/PetForm.jsx
@@ -26,7 +26,7 @@ const PetForm = ({ handleSubmit, petData, btnText }) => {
   function submit(e) {
     e.preventDefault()
     console.log(pet)
-    //handleSubmit(pet)
+    handleSubmit(pet)
   }
 
   return (
