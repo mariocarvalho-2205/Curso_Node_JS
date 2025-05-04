443a94b214e808420eae8c701fc87554460f10ad
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2025-05-04 20:36:56 -0300

    feat: aulas npm

---
+++ Aulas/13_NPM/index.js
@@ -0,0 +1,36 @@
+/*
+// npm config
+* Configurando o autor e o email
+** npm config set init-author-name NOme
+** npm config set init-author-email Email@gmail.com
+** npm config set init-author-url http://meusite.com
+
+* exibindo configurações do npm
+** npm get
+
+* apagando configuração
+** npm config delete init-author-name
+** npm config delete author
+** npm config delete keyset
+
+* instalando modulo global
+** npm --global install ou a flag -g
+
+* removendo modulos globais
+** npm remove modulo -g ou --global
+
+* listando modulos instalados
+** npm list mostra os modulos instalados no projeto
+** npm list --depth=0 mostra os modulos principais instalados do projeto
+** npm list --depth=0 -g mostra os modulos principais instalados global
+** npm list -g mostra os modulos instalados global
+** npm ls / com alias
+
+* removendo os pacotes de forma automatica
+** npm prune  - faz uma faxina das dependencias nao usadas no projeto 
+
+* procurando modulo pelo terminal
+** npm search nome da lib
+
+
+*/
